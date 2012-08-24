4c4b65bf15383e464549a8a4b4b48391ea527848
dinoTembo88 <dino.kraljevic@gmail.com>
2012-08-24 20:13:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppDino::Application.config.secret_token = '55de83aa8b43a88ce9b3f657f950e5b633de698d307fbb6fef74dc41042ed428560b9e0d841998da98c67f7b5c785d4f5fe45ef0dfc040d6772acc710923cc7e'
